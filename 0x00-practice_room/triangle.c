c56bf969f55e126ab137af6697739b444d3bbc57
Nteboheleng <ntebohelengd@gmail.com>
2023-09-14 12:45:36 -0700

    triangle challeng

---
+++ 0x00-practice_room/triangle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints a triangle
+ * Return: Always 0
+ */
+
+int main (void)
+
+{
+	printf("     *\n");
+	printf("    * *\n");
+	printf("   *   *\n");
+	printf("  *     *\n");
+	printf(" *       *\n");
+	printf("* * * * * *\n");
+
+	return (0);
+}
